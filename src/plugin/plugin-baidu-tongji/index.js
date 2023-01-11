c94b3a2dd2ba95c7331e15a83e21ccaa22a3e061
guoweiyi <2168522719@qq.com>
2023-01-11 18:44:39 +0800

    统计

--- src/plugin/plugin-baidu-tongji/index.js
+++ src/plugin/plugin-baidu-tongji/index.js
@@ -8,13 +8,8 @@ module.exports = function (context, options) {
             tagName: 'script',
             innerHTML: `
             var _hmt = _hmt || [];
-            (function() {
-              var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?c9a3849aa75f9c4a4e65f846cd1a5155";
-              hm.defer = true;
-              var s = document.getElementsByTagName("script")[0];
-              s.parentNode.insertBefore(hm, s);
-            })();
+            (function(){
+              "use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JmbcAjeX5SSnvAbl",ck:"JmbcAjeX5SSnvAbl",autoTrack:true,hashMode:true});
           `,
           },
           {
