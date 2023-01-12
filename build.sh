bb0025dfcfb3e6b10998ac23ad183ac8fe0f7a2f
guoweiyi <39240088+guoweiyi@users.noreply.github.com>
2023-01-12 09:40:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # 前端应用存在NPM依赖时的默认编译脚本
-node -v
+
 npm config set registry https://registry.npm.taobao.org --global
 npm config set ignore-engines true
 npm config set sharp_libvips_binary_host "https://npmmirror.com/mirrors/sharp-libvips"
 npm config set sharp_binary_host "https://npmmirror.com/mirrors/sharp"
-npm --registry https://registry.npm.taobao.org install 
-npm run build
+npm --registry https://registry.npm.taobao.org yarn install 
+yarn --registry https://registry.npm.taobao.org install
+yarn run build
